c34dec51b52939da96f19a4c7df393ae9cb09d8c
Amos Egel <amos.egel@kit.edu>
2017-01-04 11:28:31 +0100

    include index_conversion module and testing

---
+++ sourcefiles/index_conversion.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Functions to map the multiple coefficient indices s,p,l,m to a single index n and vice versa."""
+
+
+def multi2single(tau, l, m, lmax, mmax=None, particle_number=0, index_arrangement='stlm'):
+    """Return a unique single index for the totality of indices characterizing a svwf expansion coefficient.
+
+    input:
+    tau:                SVWF polarization (0=spherical TE, 1=spherical TM)
+    l:                  SVWF degree (1, ..., lmax)
+    m:                  SVWF order (-l,...,l)
+    lmax:               truncation degree of SVWF expansion
+    mmax:               (optional) truncation order of SVWF expansion, i.e., |m|<=mmax, default: mmax=lmax
+    particle_number:    (optional) number of particle (0,1,2,...), default=0
+    index_arrangement:  (optional) string to specify the order according to which the indices are arranged
+                        Possible choices are:
+                        'stlm' (default), which stands for 1. particle number, 2. tau, 3. l, 4. m
+                        (Other choices are not implemented at the moment.)
+    """
+    if mmax == None:
+        mmax = lmax
+
+    if index_arrangement == 'stlm':
+        # use:
+        # \sum_{l=1}^lmax (2\min(l,mmax)+1) = \sum_{l=1}^mmax (2l+1) + \sum_{l=mmax+1}^lmax (2mmax+1)
+        #                                   = 2*(1/2*mmax*(mmax+1))+mmax  +  (lmax-mmax)*(2*mmax+1)
+        #                                   = mmax*(mmax+2)               +  (lmax-mmax)*(2*mmax+1)
+
+        tau_blocksize = mmax * (mmax + 2) + (lmax - mmax) * (2 * mmax + 1)
+        s_blocksize = 2 * tau_blocksize
+        n = particle_number * s_blocksize
+        n += tau * tau_blocksize
+        if (l - 1) <= mmax:
+            n += (l - 1) * (l - 1 + 2)
+        else:
+            n += mmax * (mmax + 2) + (l - 1 - mmax) * (2 * mmax + 1)
+        n += m + min(l, mmax)
+        return n
---
+++ sourcefiles/tests/test_index_conversion.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Test the index_conversion module"""
+
+import unittest
+import index_conversion
+
+
+class IndexConversionTest(unittest.TestCase):
+    def test_multi2single_stlm(self):
+        idcs = []
+        lmax = 5
+        for s in range(3):
+            for tau in range(2):
+                for l in range(1, lmax + 1):
+                    for m in range(-l, l + 1):
+                        idcs.append(index_conversion.multi2single(particle_number=s, tau=tau, l=l, m=m, lmax=lmax))
+        self.assertEqual(idcs, list(range(len(idcs))))
+
+        idcs = []
+        lmax = 6
+        mmax = 3
+        for s in range(3):
+            for tau in range(2):
+                for l in range(1, lmax + 1):
+                    mlim = min(l, mmax)
+                    for m in range(-mlim, mlim + 1):
+                        idcs.append(
+                            index_conversion.multi2single(particle_number=s, tau=tau, l=l, m=m, lmax=lmax, mmax=mmax))
+        self.assertEqual(idcs, list(range(len(idcs))))
+
+
+if __name__ == '__main__':
+    unittest.main()
