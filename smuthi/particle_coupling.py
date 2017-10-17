06d3f5fbf60e4044ce350e33878098ddd5d24bb7
Amos Egel <amos.egel@kit.edu>
2017-10-17 17:17:48 +0200

    particle coupling 2D lookup (untested)

--- smuthi/particle_coupling.py
+++ smuthi/particle_coupling.py
@@ -11,6 +11,11 @@ import smuthi.vector_wave_functions as vwf
 import matplotlib.pyplot as plt
 
 
+radial_distance_array = None
+radial_distance_table = None
+radial_particle_coupling_lookup = None
+
+
 def layer_mediated_coupling_block(vacuum_wavelength, receiving_particle, emitting_particle, layer_system, 
                                   k_parallel='default', show_integrand=False):
     """Layer-system mediated particle coupling matrix :math:`W^R` for two particles. This routine is explicit, but slow.
@@ -266,3 +271,128 @@ def direct_coupling_matrix(vacuum_wavelength, particle_list, layer_system):
             w[idx1[:, None], idx2] = direct_coupling_block(vacuum_wavelength, particle1, particle2, layer_system)
 
     return w
+
+
+def radial_coupling_lookup(vacuum_wavelength, particle_list, layer_system, k_parallel='default', resolution=None):
+    
+    global radial_distance_array
+    global radial_distance_table
+    global particle_coupling_lookup_2D
+    
+    if resolution is None:
+        resolution = vacuum_wavelength / 100
+    
+    l_max = max([particle.l_max for particle in particle_list])
+    m_max = max([particle.m_max for particle in particle_list])
+    n_max = fldex.blocksize(l_max, m_max)
+    max_rho = max([np.sqrt(particle.position[0] ** 2 + particle.position[1] ** 2) for particle in particle_list])
+    
+    radial_distance_array = np.arange(0, 2 * (max_rho + resolution), resolution)
+    
+    radial_distance_table = []
+    for i1, particle1 in enumerate(particle_list):
+        radial_distance_table.append([])
+        for i2, particle2 in enumerate(particle_list):
+            radial_distance_table[i1].append(np.sqrt((particle1.position[0] - particle2.position[0])**2 
+                                                     + (particle1.position[1] - particle2.position[1])**2))
+    
+    z = particle_list[0].position[2]
+    i_s = layer_system.layer_number(z)
+    k_is = layer_system.wavenumber(i_s, vacuum_wavelength)
+    dz = z - layer_system.reference_z(i_s) 
+        
+    lookup_table = np.zeros((n_max, n_max, len(radial_distance_array)), dtype=complex)
+    
+    # direct -----------------------------------------------------------------------------------------------------------
+    w = np.zeros((n_max, n_max, len(radial_distance_array)), dtype=complex)
+
+    ct = 0
+    st = 1
+    bessel_h = [sf.spherical_hankel(n, k * radial_distance_array) for n in range(2* l_max + 1)]
+    legendre, _, _ = sf.legendre_normalized(ct, st, 2 * l_max)
+
+    for m1 in range(-m_max, m_max + 1):
+        for m2 in range(-m_max, m_max + 1):
+            for l1 in range(max(1, abs(m1)), l_max + 1):
+                for l2 in range(max(1, abs(m2)), l_max + 1):
+                    A = np.zeros(len(radial_distance_array), dtype=complex)
+                    B = np.zeros(len(radial_distance_array), dtype=complex)
+                    for ld in range(max(abs(l1 - l2), abs(m1 - m2)), l1 + l2 + 1):  # if ld<abs(m1-m2) then P=0
+                        a5, b5 = vwf.ab5_coefficients(l2, m2, l1, m1, ld)
+                        A += a5 * bessel_h[ld] * legendre[ld][abs(m1 - m2)]
+                        B += b5 * bessel_h[ld] * legendre[ld][abs(m1 - m2)]
+                    for tau1 in range(2):
+                        n1 = fldex.multi_to_single_index(tau1, l1, m1, l_max, m_max)
+                        for tau2 in range(2):
+                            n2 = fldex.multi_to_single_index(tau2, l2, m2, l_max, m_max)
+                            if tau1 == tau2:
+                                w[n1, n2, :] = A  # remember that w = A.T
+                            else:
+                                w[n1, n2, :] = B
+    
+    # layer mediated ---------------------------------------------------------------------------------------------------
+    if type(k_parallel) == str and k_parallel == 'default':
+        k_parallel = coord.default_k_parallel
+    kz_is = coord.k_z(k_parallel=k_parallel, k=k_is)
+    len_kp = len(k_parallel)
+    wr_lookup_integrand = np.ones((n_max, n_max, len(radial_distance_array), len_kp), dtype=complex)
+    
+    # phase factors
+    ejkz = np.zeros((2, len_k), dtype=complex)  # pl/mn, kp
+    ejkz[0, :] = np.exp(1j * kz_is * dz)
+    ejkz[1, :] = np.exp(- 1j * kz_is * dz)
+        
+    # layer response
+    L = np.zeros((2, 2, 2, 1, 1, len_kp), dtype=complex)  # pol, pl/mn1, pl/mn2, kp
+    for pol in range(2):
+        L[pol, :, :, :] = lay.layersystem_response_matrix(pol, layer_system.thicknesses, 
+                                                          layer_system.refractive_indices, k_parallel,
+                                                          coord.angular_frequency(vacuum_wavelength), is2, is1)
+    
+    # transformation coefficients
+    B_dag = np.zeros((2, 2, n_max, len_kp), dtype=complex)  # pol, pl/mn, n, kp
+    B = np.zeros((2, 2, n_max, len_kp), dtype=complex)  # pol, pl/mn, n, kp
+    ct = kz_is / k_is
+    st = k_parallel / k_is
+    _, pilm_pl, taulm_pl = sf.legendre_normalized(ct, st, lmax1)
+    _, pilm_mn, taulm_mn = sf.legendre_normalized(-ct, st, lmax1)
+    pilm = (pilm_list_pl, pilm_list_mn)
+    taulm = (taulm_list_pl, taulm_list_mn)
+    
+    for tau in range(2):
+        for m in range(-m_max, m_max + 1):
+            for l in range(max(1, abs(m)), l_max + 1):
+                n = fldex.multi_to_single_index(tau, l, m, l_max, m_max)
+                m_vec[n] = m
+                for pol in range(2):
+                    B_dag[pol, 0, n, :] = vwf.transformation_coefficients_vwf(tau, l, m, pol, pilm_list=pilm_pl, 
+                                                                                    taulm_list=taulm_pl, dagger=True)
+                    B_dag[pol, 1, n, :] = vwf.transformation_coefficients_vwf(tau, l, m, pol, pilm_list=pilm_mn, 
+                                                                                    taulm_list=taulm_mn, dagger=True)
+                    B[pol, 0, n, :] = vwf.transformation_coefficients_vwf(tau, l, m, pol, pilm_list=pilm_pl, 
+                                                                             taulm_list=taulm_pl, dagger=False)
+                    B[pol, 1, n, :] = vwf.transformation_coefficients_vwf(tau, l, m, pol, pilm_list=pilm_mn, 
+                                                                             taulm_list=taulm_mn, dagger=False)
+                    
+    bel = np.zeros((2, 2, n_max, len_k), dtype=complex)  # pol, pl/mn2, n1, kp
+    for pm1 in range(2):
+        for pol in range(2):
+            bl[pol, :, :, :] += L[pol, pm1,  :, None, :] * B_dag[pol, pm1, None, :, :] * ejkz[pm1, None, None, :]
+                                  
+    belbe = np.zeros((n_max, n_max, len_k), dtype=complex)  # n1, n2, kp
+    for pm2 in range(2):
+        for pol in range(2):
+            belbe += bel[pol, pm2, :, None, :] * B[pol, pm2, None, :, :] * ejkz[1 - pm2, None, None, :] 
+                                
+    # bessel function and jacobi factor
+    bessel_list = []
+    for dm in range(2 * l_max + 1):
+        bessel_list.append(scipy.special.jv(dm, k_parallel[None, :] * radial_distance_array[:, None]))
+    bessel_full = np.array([[bessel_list[abs(m_vec[n1] - m_vec[n2])] for n2 in range(n_max)] for n1 in range(n_max)])
+    jacobi_vector = k_parallel / (kz_is * k_is)
+    integrand = bessel_full * jacobi_vector[None, None, None, :] * belbe[:, :, None, :]  # n1, n2, rho, kp
+    integral = np.trapz(integrand, x=k_parallel, axis=-1)  # n1, n2, rho
+    m2_minus_m1 = m_vec[None, :, None] - m_vec[:, None, None]
+    wr = 4 * (1j) ** abs(m2_minus_m1) * integral
+    
+    return w + wr
