9638dace76e426a7a05b7e60ad999c2eb4058db8
Amos Egel <amos.egel@kit.edu>
2017-11-24 11:20:55 +0100

    allow nmfds installation in existing folder

--- smuthi/nfmds/__init__.py
+++ smuthi/nfmds/__init__.py
@@ -1,26 +1,25 @@
 """Check if NFM-DS is installed and otherwise do so."""
 import pkg_resources
-import shutil
 import os
 import subprocess
 import sys
-
+from distutils.dir_util import copy_tree
 
 nfmds_sources_dirname = pkg_resources.resource_filename('smuthi.nfmds', 'NFM-DS')
-with open(nfmds_sources_dirname + '/_nfmds_installation_path.txt', 'r') as rfile:
-    nfmds_installation_path = rfile.read()
 
-
-def install_nfmds():
-    global nmfds_installation_path
+try:
+    with open(nfmds_sources_dirname + '/_nfmds_installation_path.txt', 'r') as rfile:
+        nfmds_installation_path = rfile.read()
+except:  # install
     nfmds_installation_path = os.path.abspath(input('Please type a path where NFM-DS will be installed!\n') + '/NFM-DS')
-    shutil.copytree(nfmds_sources_dirname, nfmds_installation_path)
+    copy_tree(nfmds_sources_dirname, nfmds_installation_path)
     try:
         with open(nfmds_sources_dirname + '/_nfmds_installation_path.txt', 'w') as wfile:
             wfile.write(nfmds_installation_path)
     except:
-        print('Permission denied: Could not write to ' + nfmds_sources_dirname + '/_nfmds_installation_path.txt.')
+        print('Could not write to ' + nfmds_sources_dirname + '/_nfmds_installation_path.txt.')
         print('Consider to run once with admin rights to keep NFM-DS installation.')
+
     if sys.platform.startswith('linux'):
         cwd = os.getcwd()
         os.chdir(nfmds_installation_path + '/TMATSOURCES')
--- smuthi/nfmds/t_matrix_axsym.py
+++ smuthi/nfmds/t_matrix_axsym.py
@@ -16,11 +16,6 @@ import imp
 import sys
 
 
-if smuthi.nfmds.nfmds_installation_path == '':
-    smuthi.nfmds.install_nfmds()
-    imp.reload(smuthi.nfmds)
-
-
 def tmatrix_spheroid(vacuum_wavelength=None, layer_refractive_index=None, particle_refractive_index=None,
                      semi_axis_c=None, semi_axis_a=None, l_max=None, m_max=None, use_ds=True, nint=None, nrank=None):
     """T-matrix for spheroid, using the TAXSYM.f90 routine from the NFM-DS.
