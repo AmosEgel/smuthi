f7d12c511ddc12b0931870ebd8ec7940f174abb1
Amos Egel <amos.egel@kit.edu>
2019-11-08 22:23:48 +0100

    illustrations and discussion for many spheres tutorial

--- docs/examples/many_particles/discussion.rst
+++ docs/examples/many_particles/discussion.rst
@@ -1,13 +1,46 @@
 Many spheres on a substrate
 ===========================
 
-.. image:: vogel_spiral_1000.png
-   :width: 50%
-   :align: right
-
 This tutorial demonstrates how to set up a simulation containing many particles.
+
+Configuration
+-------------
 The configuration under study consists of a number of dielectric spheres that are
 arranged in the shape of a spiral on a glass substrate.
 
-The spheres are illuminated by a plane wave under normal incidence.
+.. image:: vogel_spiral_200.png
+   :width: 45%
+
+.. image:: drawing.png
+   :width: 45%
+
+The spheres are illuminated by a plane wave from top under normal incidence.
+
+How to compute large systems
+----------------------------
+In order to limit the runtime, Smuthi offers various numerical strategies for
+the solution of the scattering problem.
+
+To do: complete the discussion
+
+.. image:: cross_section.png
+   :width: 75%
+   :align: center
+
+.. image:: runtime.png
+   :width: 75%
+   :align: center
+
+Exemplary far field results
+---------------------------
+
+.. image:: dscs_200spheres_top.png
+   :scale: 50%
+
+.. image:: dscs_200spheres_bottom.png
+   :scale: 50%
+
+The above images show the resulting differential scattering cross section for a spiral of 200 spheres in the top hemisphere
+(reflection) and in the bottom hemisphere (transmission).
+
 
--- examples/tutorials/many_spheres_on_substrate/many_dielectric_spheres_on_substrate.py
+++ examples/tutorials/many_spheres_on_substrate/many_dielectric_spheres_on_substrate.py
@@ -5,32 +5,21 @@
 # incidence.                                                                  #
 #*****************************************************************************#
 
-import time
-import tempfile
 import numpy as np
-import shutil
 import matplotlib.pyplot as plt
-import imageio
 import smuthi.simulation
 import smuthi.initial_field
 import smuthi.layers
 import smuthi.particles
 import smuthi.postprocessing.far_field
 import smuthi.postprocessing.graphical_output
-import smuthi.fields.coordinates_and_contours as coord
 import smuthi.utility.cuda as cu
 
 
 # In this file, all lengths are given in nanometers
 
-coord.set_default_k_parallel(vacuum_wavelength=550,
-                             neff_resolution=5e-3,
-                             neff_max=2.5,
-                             neff_imag=1e-2)
-
-
+# particle configuration
 def vogel_spiral(number_of_spheres):
-    # Scattering particles (Vogel spiral)
     spheres_list = []
     for i in range(1, number_of_spheres):
         r = 200 * np.sqrt(i)
@@ -44,13 +33,15 @@ def vogel_spiral(number_of_spheres):
     return spheres_list
 
 
+# set up and run a simulation
 def simulate_N_spheres(number_of_spheres=100,
                        direct_inversion=True,
                        use_gpu=False,
                        solver_tolerance=5e-4,
                        lookup_resolution=5,
-                       interpolation_order='linear'):
-    
+                       interpolation_order='linear',
+                       make_illustrations=False):
+
     # Initialize the layer system: substrate (glass) and ambient (air)
     two_layers = smuthi.layers.LayerSystem(thicknesses=[0, 0],
                                            refractive_indices=[1.52, 1])
@@ -69,14 +60,10 @@ def simulate_N_spheres(number_of_spheres=100,
         print("Failed to load pycuda, skipping simulation")
         return [0, 0, 0]
    
-    preparation_time = 0
-    solution_time = 0
-
-    simulation = None
     if direct_inversion:
         simulation = smuthi.simulation.Simulation(layer_system=two_layers,
-                                              particle_list=spheres_list,
-                                              initial_field=plane_wave)
+                                                  particle_list=spheres_list,
+                                                  initial_field=plane_wave)
     else:
         simulation = smuthi.simulation.Simulation(layer_system=two_layers,
                                                   particle_list=spheres_list,
@@ -87,21 +74,7 @@ def simulate_N_spheres(number_of_spheres=100,
                                                   coupling_matrix_lookup_resolution=lookup_resolution,
                                                   coupling_matrix_interpolator_kind=interpolation_order)
 
-    # Here, we could start the simulation by 'simulation.run()'
-    # This command triggers the preparation and solution of the linear system.
-    # However, we will not use 'simulation.run()', but rather trigger the
-    # assembly and solution of the linear system manually in order to measure
-    # the time.
-    start = time.time()
-    simulation.initialize_linear_system()
-    simulation.linear_system.prepare()
-    end = time.time()
-    preparation_time = end - start
-
-    start = time.time()
-    simulation.linear_system.solve()
-    end = time.time()
-    solution_time = end - start
+    preparation_time, solution_time, _ = simulation.run()
 
     # compute cross section
     ecs = smuthi.postprocessing.far_field.extinction_cross_section(
@@ -109,49 +82,41 @@ def simulate_N_spheres(number_of_spheres=100,
         particle_list=spheres_list,
         layer_system=two_layers)
 
+    if make_illustrations:
+        azimuthal_angles = np.arange(0, 361, 0.5, dtype=float) * np.pi / 180
+        polar_angles = np.arange(0, 181, 0.25, dtype=float) * np.pi / 180
+        dscs = smuthi.postprocessing.far_field.scattering_cross_section(
+            initial_field=plane_wave,
+            particle_list=spheres_list,
+            layer_system=two_layers,
+            polar_angles=polar_angles,
+            azimuthal_angles=azimuthal_angles,
+        )
+
+        # display differential scattering cross section
+        smuthi.postprocessing.graphical_output.show_far_field(
+            far_field=dscs,
+            save_plots=True,
+            show_plots=True,
+            save_data=False,
+            tag='dscs_%ispheres'%number_of_spheres,
+            log_scale=True)
+
+        xlim = max([abs(sphere.position[0]) for sphere in spheres_list]) * 1.1
+        plt.figure()
+        plt.xlim([-xlim, xlim])
+        plt.xlabel('x (nm)')
+        plt.ylabel('y (nm)')
+        plt.ylim([-xlim, xlim])
+        plt.gca().set_aspect("equal")
+        plt.title("Vogel spiral with %i spheres" % number_of_spheres)
+        smuthi.postprocessing.graphical_output.plot_particles(
+            -1e5, 1e5, -1e5, 1e5, 0, 0, spheres_list, 1000, False)
+        plt.savefig("vogel_spiral_%i.png" % number_of_spheres)
+
     return [(ecs["top"] + ecs["bottom"]).real, preparation_time, solution_time]
-    
-# display spheres configuration
-limit = 7000
-number_of_spheres = 1000
-spheres_list = vogel_spiral(number_of_spheres)
-plt.xlim([-limit, limit])
-plt.xlabel('x (nm)')
-plt.ylabel('y (nm)')
-plt.ylim([-limit, limit])
-plt.gca().set_aspect("equal")
-plt.title("Vogel spiral with %i spheres"%number_of_spheres)
-smuthi.postprocessing.graphical_output.plot_particles(
-    -limit, limit, -limit, limit, 0, 0, spheres_list, 1000, False)
-plt.savefig("vogel_spiral_%i.png"%number_of_spheres)
-plt.show()
 
-# create gif
-sphere_numbers = [5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
-tempdir = tempfile.mkdtemp()
-images = []
-limit = 14000
-for number_of_spheres in sphere_numbers:
-    spheres_list = vogel_spiral(number_of_spheres)
-    tempfig = plt.figure()
-    plt.xlim([-limit, limit])
-    plt.xlabel('x (nm)')
-    plt.ylabel('y (nm)')
-    plt.ylim([-limit, limit])
-    plt.gca().set_aspect("equal")
-    plt.title("Vogel spiral with %i spheres"%number_of_spheres)
-    smuthi.postprocessing.graphical_output.plot_particles(
-        -limit, limit, -limit, limit, 0, 0, spheres_list, 1000, False)
-    #plt.savefig("vogel_spiral_%i.png"%number_of_spheres)
-    tempfig_filename = tempdir + '/temp_' + str(number_of_spheres) + '.png'
-    plt.savefig(tempfig_filename)
-    plt.close(tempfig)
-    images.append(imageio.imread(tempfig_filename))
-
-imageio.mimsave('Vogel_spirals.gif', images, duration=0.5)
-shutil.rmtree(tempdir)
-plt.show()
-
+# -----------------------------------------------------------------------------
 # launch a series of simulations:
 
 # iterative solution on GPU
@@ -170,7 +135,7 @@ for particle_number in gpu_iterative_particle_numbers:
     gpu_iterative_ecs.append(results[0])
 
 # direct solution on CPU
-cpu_direct_particle_numbers = [5, 10, 20, 50]
+cpu_direct_particle_numbers = [5, 10, 20, 50, 100]
 cpu_direct_times = []
 cpu_direct_preptimes = []
 cpu_direct_ecs = []
@@ -185,7 +150,7 @@ for particle_number in cpu_direct_particle_numbers:
     cpu_direct_ecs.append(results[0])
 
 # iterative solution on CPU
-cpu_iterative_particle_numbers = [5, 10, 20, 50, 100, 200]
+cpu_iterative_particle_numbers = [5, 10, 20, 50, 100, 200, 500]
 cpu_iterative_times = []
 cpu_iterative_preptimes = []
 cpu_iterative_ecs = []
@@ -199,11 +164,19 @@ for particle_number in cpu_iterative_particle_numbers:
     cpu_iterative_preptimes.append(results[1])
     cpu_iterative_ecs.append(results[0])
 
+# -----------------------------------------------------------------------------
+# plot the results
+
 # get GPU device name
-import pycuda.driver as drv
-drv.init()
-device_name = drv.Device(0).name()   
-   
+device_name = "no gpu"
+try:
+    import pycuda.driver as drv
+    drv.init()
+    device_name = drv.Device(0).name()
+except:
+    pass
+
+# plot runtime
 plt.figure()
 plt.xlabel("Number of spheres")
 plt.ylabel("Solver time")
@@ -213,10 +186,12 @@ plt.loglog(cpu_iterative_particle_numbers, cpu_iterative_times, '-ro')
 plt.loglog(cpu_iterative_particle_numbers, cpu_iterative_preptimes, '--ro')
 plt.loglog(gpu_iterative_particle_numbers, gpu_iterative_times, '-gd')
 plt.loglog(gpu_iterative_particle_numbers, gpu_iterative_preptimes, '--gd')
-plt.legend(["direct, CPU, total", "direct, CPU, prep.", "iter., CPU, total", "iter., CPU, prep.", "iter., GPU, total", "iter., GPU, prep."])
+plt.legend(["direct, CPU, total", "direct, CPU, prep.", "iter., CPU, total", "iter., CPU, prep.", "iter., GPU, total",
+            "iter., GPU, prep."])
 plt.grid()
 plt.savefig("runtime.png")
 
+# plot extinction cross section
 plt.figure()
 plt.xlabel("Number of spheres")
 plt.ylabel("Extinction cross section [nm^2]")
@@ -227,5 +202,11 @@ plt.legend(["Direct solution on CPU", "Iterative solution on CPU", "Iterative so
 plt.grid()
 plt.savefig("cross_section.png")
 
-plt.show()
-
+# create illustrations
+# check if gpu can be enabled
+cu.enable_gpu()
+gpu_enabled = cu.use_gpu
+results = simulate_N_spheres(number_of_spheres=200,
+                             direct_inversion=False,
+                             use_gpu=gpu_enabled,
+                             make_illustrations=True)
